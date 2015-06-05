bdd174db298803087e9dee9e12bee338062d8319
Luis Aurelio Palomá Meza <luispaloma3@gmail.com>
2015-06-05 13:30:30 -0500

    Version mobile
    Finalización version mobile

--- public/js/script.js
+++ public/js/script.js
@@ -19,6 +19,24 @@ var MenuLoader = (function() {
 		return html;
 	}
 
+	function _createMobileMenu(json) {
+		var html = '';
+
+		for ( var obj  in json.items ) {
+			html += '<li class="MobileMenu-item ">';
+			html += '	<a href="' + json.items[obj].url + '" title="' + json.items[obj].label + '" class="MobileMenu-item-link ' + (typeof json.items[obj].items !== 'undefined' && json.items[obj].items.length > 0 ? 'MobileMenu-item-link--hasSub' : '' ) +'">';
+			html += '		' + json.items[obj].label;
+			html += '	</a>';
+			if ( typeof json.items[obj].items !== 'undefined' && json.items[obj].items.length > 0 ) {
+				html += '<ul class="MobileMenu-subMenu">';
+				html += _createMobileMenu(json.items[obj]);
+				html +='</ul>';
+			}
+			html += '</li>';
+		}
+		return html;
+	}
+
 	function _ajax(url) {
 		var ajaxrequest = new XMLHttpRequest();
 
@@ -27,22 +45,31 @@ var MenuLoader = (function() {
 		return JSON.parse(ajaxrequest.responseText);
 	}
 
-	function _setHtml(htmlMenu) {
-		var ulTag = document.getElementById('MainMenu');
+	function _setHtml(htmlMenu, element) {
+		var ulTag = document.getElementById(element);
 		ulTag.innerHTML = ulTag.innerHTML+htmlMenu;
 	}
 
 	function _addEventListener(){
 		var li = document.querySelectorAll('#MainMenu > .MainMenu-item > a');
+		var liMobile = document.querySelectorAll('#MobileMenu > .MobileMenu-item');
 		var mask = document.getElementsByClassName('Mask');
 		var body = document.getElementsByTagName('body');
+		var menuMobile = document.getElementsByClassName('MobileClose');
+		var menuBurger = document.getElementsByClassName('BurgerIcon-icon');
 
 		for (var i = 0; i < li.length; i++) {
 			li[i].addEventListener('click', handleClicks);
 		}
 
+		for ( i = 0; i < li.length; i++) {
+			liMobile[i].addEventListener('click', handleClicksMobile);
+		}
+
 		mask[0].addEventListener('click', handleMaskClick);
 		body[0].addEventListener('click', handleBodyClick);
+		menuMobile[0].addEventListener('click', handleMenuMobileClick);
+		menuBurger[0].addEventListener('click', handleMenuBurgerClick);
 	}
 
 	function handleClicks() {
@@ -57,6 +84,15 @@ var MenuLoader = (function() {
 		}
 		
 		this.classList.add('is-hovered');
+	}
+	function handleClicksMobile() {
+		var liHovered = document.getElementsByClassName('is-hoveredm');
+				
+		if ( typeof liHovered[0] !== 'undefined' ) {
+			liHovered[0].classList.remove('is-hoveredm');
+		}
+		
+		this.classList.add('is-hoveredm');
 	}
 
 	function handleMaskClick() {
@@ -75,14 +111,25 @@ var MenuLoader = (function() {
 				liHovered[j].classList.remove('is-hovered');
 			}
 		}
-		
+	}
+
+	function handleMenuMobileClick (event) {
+		var menuMobile = document.getElementsByClassName('Mobile');
+		menuMobile[0].classList.remove('is-shown');
+	}
+
+	function handleMenuBurgerClick (event) {
+		var menuMobile = document.getElementsByClassName('Mobile');
+		menuMobile[0].classList.add('is-shown');
 	}
 
 	function init() {
 		var url = '/api/nav.json';
 		var jsonData = _ajax(url);
 		var htmlMenu = _createMenu(jsonData);
-		_setHtml(htmlMenu);
+		var htmlMobileMenu = _createMobileMenu(jsonData);
+		_setHtml(htmlMenu, 'MainMenu');
+		_setHtml(htmlMobileMenu, 'MobileMenu');
 		_addEventListener();
 	}
 
